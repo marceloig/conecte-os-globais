6210cc4e2a41efb5093c2703c9d5eac5df23ab49
Igor Oliveira <cccmigor@gmail.com>
2025-08-31 16:46:03 -0300

    background de tv com barras horizontais por CSS

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -172,7 +172,7 @@ function App() {
                   {isLoadingNewGame ? 'Carregando...' : 'Novo jogo'}
                 </Button>
               </Panel>
-              <Background variant={BackgroundVariant.Cross} gap={12} size={1} className="tv-bars-background" />
+              <Background variant={BackgroundVariant.Cross} gap={12} size={1} className="tv-background bars-only" />
             </ReactFlow>
             <ModalEndGame open={openDialog} onOpenChange={setOpenDialog} graph={graph} />
           </ReactFlowProvider>
